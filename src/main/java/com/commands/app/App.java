9102c77472e8f5a060813be75d792d7d7e4af1f2
Pundir <kunalpundir111@gmail.com>
2023-09-25 19:59:46 +0530

    project setup

---
+++ src/main/java/com/commands/app/App.java
@@ -0,0 +1,16 @@
+package com.commands.app;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ *
+ */
+@Slf4j
+public class App 
+{
+    public static void main( String[] args )
+    {
+        log.info("Running word-count shell command using java....");
+    }
+}
